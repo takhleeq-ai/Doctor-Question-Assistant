7a8bf52cbb46323c8f33d8673666d4e850362009
Takhleeq.ai <subhan.ahsan@gmail.com>
2026-01-23 11:18:06 +0000

    Respect Vite base path in router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -136,15 +136,22 @@ function AppContent() {
 }
 
 function App() {
+  const baseUrl =
+    import.meta.env.BASE_URL === "/"
+      ? "/"
+      : import.meta.env.BASE_URL.replace(/\/$/, "");
+
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="light" storageKey="healthprep-theme">
-        <TooltipProvider>
-          <AppContent />
-          <Toaster />
-          <UpdateNotification />
-        </TooltipProvider>
-      </ThemeProvider>
+      <WouterRouter base={baseUrl}>
+        <ThemeProvider defaultTheme="light" storageKey="healthprep-theme">
+          <TooltipProvider>
+            <AppContent />
+            <Toaster />
+            <UpdateNotification />
+          </TooltipProvider>
+        </ThemeProvider>
+      </WouterRouter>
     </QueryClientProvider>
   );
 }
